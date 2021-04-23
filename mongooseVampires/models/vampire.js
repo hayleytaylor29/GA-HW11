7f65f245c19f4ecfaa7381bc720edb4d8bdc592f
hayleytaylor29 <hft929@gmail.com>
2021-04-22 21:59:34 -0400

    Set up files and schema, returned vampire list in terminal with all data correctly.

---
+++ mongooseVampires/models/vampire.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const vampireSchema = new Schema({
+  // add your code here to set up your schema
+  name: {type: String, required: true, unique: true},
+  dob: {type: Date, default: Date.now},
+  hair_color: {type: String, default: 'Blonde'},
+  eye_color: {type: String},
+  loves: [{type: String}],
+  location: {type: String},
+  gender: {type: String},
+  victims: {type: Number, min: 1}
+
+});
+
+const Vampire = mongoose.model('Vampire', vampireSchema);
+
+//make this exportable to be accessed in `app.js`
+module.exports = Vampire;
