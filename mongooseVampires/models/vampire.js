3131c8d7ddc4d865341e343967f7b189dfc7a45b
hayleytaylor29 <hft929@gmail.com>
2021-04-23 23:15:33 -0400

    all commands completed in 'commands.txt' file

--- mongooseVampires/models/vampire.js
+++ mongooseVampires/models/vampire.js
@@ -4,14 +4,14 @@ const Schema = mongoose.Schema;
 const vampireSchema = new Schema({
   // add your code here to set up your schema
   name: {type: String, required: true, unique: true},
-  dob: {type: Date, default: Date.now},
+  title: {type: String},
+  dob: {type: Date, default: Date.now()},
   hair_color: {type: String, default: 'Blonde'},
   eye_color: {type: String},
   loves: [{type: String}],
   location: {type: String},
   gender: {type: String},
   victims: {type: Number, min: 1}
-
 });
 
 const Vampire = mongoose.model('Vampire', vampireSchema);
